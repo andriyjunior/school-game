78350fdad589b3b5dd53ac6cf04e1e4f2a881d54
Andrii <andriyjunior14@gmail.com>
2025-11-19 18:06:31 +0200

    works

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === 'production' ? '/school-game/' : '/',
-  build: {
-    outDir: 'dist',
-    assetsDir: 'assets',
-    sourcemap: false,
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          redux: ['@reduxjs/toolkit', 'react-redux']
+export default defineConfig(({mode}) => ({
+    plugins: [react()],
+    base: '/',
+    build: {
+        outDir: 'dist',
+        assetsDir: 'assets',
+        sourcemap: false,
+        rollupOptions: {
+            output: {
+                manualChunks: {
+                    vendor: ['react', 'react-dom', 'react-router-dom'],
+                    redux: ['@reduxjs/toolkit', 'react-redux']
+                }
+            }
         }
-      }
+    },
+    server: {
+        port: 3321,
+        open: true
     }
-  },
-  server: {
-    port: 3321,
-    open: true
-  }
 }))
